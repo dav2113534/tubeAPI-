716a32184d283b7c67640d5f5b8245cef7932595
dav2113534 <dpalacios03081991@gmail.com>
2017-03-26 20:23:47 -0700

    added watchSubmit function

--- tube.js
+++ tube.js
@@ -5,7 +5,32 @@ var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
 function getDataFromApi() {
     var getJson = {
         part: "snippet",
-        key: "AIzaSyCxu-HaWg7nUN9KkUD3ozKgOQdZHU3Pyy0"
+        key: "AIzaSyCxu-HaWg7nUN9KkUD3ozKgOQdZHU3Pyy0",
+        q: "Dave chappelle"
     }
     $.getJson(youtubeUrl, getJson)
 }
+
+function displayData(data) {
+    var result = '';
+    if (data.Search) {
+        data.Search.forEach(function (item) {
+            result += '<p>' + item.Title + '</p>';
+        });
+    } else {
+        result += '<p>No results</p>';
+    }
+    $('.query').html(result);
+}
+
+function watchSubmit() {
+    $('.js-search-form').submit(function (e) {
+        e.preventDefault();
+        var query = $(this).find('.js-query').val();
+        getDataFromApi(getJson, displayData);
+    });
+}
+
+$(function () {
+    watchSubmit();
+});
