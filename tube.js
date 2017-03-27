ac87549029a965aafbc19c6739f6d9fbea1aa3ed
dav2113534 <dpalacios03081991@gmail.com>
2017-03-27 11:38:05 -0700

    added parameters for api function

--- tube.js
+++ tube.js
@@ -1,33 +1,33 @@
-var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search';
+var youtubeUrl = "https://www.googleapis.com/youtube/v3/search";
 
 //AIzaSyCxu-HaWg7nUN9KkUD3ozKgOQdZHU3Pyy0 API Key
 
-function getDataFromApi() {
+function getDataFromApi(query, callback) {
     var getJson = {
         part: "snippet",
         key: "AIzaSyCxu-HaWg7nUN9KkUD3ozKgOQdZHU3Pyy0",
-        q: "Dave chappelle"
+        q: ""
     }
-    $.getJson(youtubeUrl, getJson)
+    $.getJSON(youtubeUrl, getJson, callback)
 }
 
 function displayData(data) {
     var result = '';
-    if (data.Search) {
-        data.Search.forEach(function (item) {
-            result += '<p>' + item.Title + '</p>';
+    if (data.items) {
+        data.items.forEach(function (item) {
+            result += '<p>' + item.snippet.title + '</p>';
         });
     } else {
         result += '<p>No results</p>';
     }
-    $('.query').html(result);
+    $('.js-search-results').html(result);
 }
 
 function watchSubmit() {
     $('.js-search-form').submit(function (e) {
         e.preventDefault();
         var query = $(this).find('.js-query').val();
-        getDataFromApi(getJson, displayData);
+        getDataFromApi(query, displayData);
     });
 }
 
